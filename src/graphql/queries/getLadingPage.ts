9cc9e0d07f9edb98145c1fc098b06a18825724cd
Cristian Araujo <araujocristian@live.com>
2021-07-09 12:19:35 -0300

    feat: Configurando Graphql

---
+++ src/graphql/queries/getLadingPage.ts
@@ -0,0 +1,36 @@
+const GET_LANDING_PAGE = /* GraphQL */ `
+  fragment imageData on UploadFile {
+    alternativeText
+    url
+  }
+
+  fragment logo on LandingPage {
+    logo {
+      ...imageData
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
+        ...imageData
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      createdAt: created_at
+      ...logo
+      ...header
+    }
+  }
+`
+
+export default GET_LANDING_PAGE
